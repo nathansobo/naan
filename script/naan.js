88370b96cec506b63e95a95a4d3b519d269e2d47
Nathan Sobo <nathansobo@gmail.com>
2010-08-10 22:49:29 -0400

    Played with the image movement

--- script/naan.js
+++ script/naan.js
@@ -1,13 +1,14 @@
 $(function() {
   var options = {
-    fade: 1
+    fade: 1,
+    variant: true
   };
   
   var slides = [
-    { src:  'images/food/aloo_gobi.png', from: '50% 40% 1x', to: '100% 100% 1x', time: 5 },
-    { src:  'images/food/spices.jpg', from: '100% 100% 1x', to: '20% 20% 1x', time: 5 },
-    { src:  'images/food/naan.jpg', from: '100% 100% 1x', to: '100% 100% 1x', time: 5 },
-    { src:  'images/food/kale_curry.jpg', from: '50% 40% 1x', to: '100% 100% 1x', time: 5 }
+    { src:  'images/food/aloo_gobi.png', from: '20% 20% 1x', to: '80% 100% 1x', time: 8 },
+    { src:  'images/food/spices.jpg', from: '100% 100% 1x', to: '0% 0% 1x', time: 8 },
+    { src:  'images/food/naan.jpg', from: '0% 0% 1x', to: '0% 0% 1x', time: 8 },
+    { src:  'images/food/kale_curry.jpg', from: '15% 15% 1x', to: '100% 70% 1x', time: 8 }
   ];
 
   $("#slideshow").crossSlide(options, slides);
