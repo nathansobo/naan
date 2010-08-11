b5cb718630a0c16f19bd4762c818eb68b945951a
Nathan Sobo <nathansobo@gmail.com>
2010-08-10 22:25:28 -0400

    Incorporated better images into the slideshow

--- script/naan.js
+++ script/naan.js
@@ -1,21 +1,13 @@
 $(function() {
   var options = {
-    fade: 1,
-    sleep: 7
+    fade: 1
   };
-
+  
   var slides = [
-    {
-      src:  'images/food/spices.jpg',
-    }, {
-      src:  'images/food/chana_masala.jpg',
-    }, {
-      src:  'images/food/paneer.jpg',
-    }, {
-      src:  'images/food/bhindi.jpg',
-    },{
-      src:  'images/food/samosas.jpg',
-    }
+    { src:  'images/food/aloo_gobi.png', from: '50% 40% 1x', to: '100% 100% 1x', time: 5 },
+    { src:  'images/food/spices.jpg', from: '100% 100% 1x', to: '20% 20% 1x', time: 5 },
+    { src:  'images/food/naan.jpg', from: '100% 100% 1x', to: '100% 100% 1x', time: 5 },
+    { src:  'images/food/kale_curry.jpg', from: '50% 40% 1x', to: '100% 100% 1x', time: 5 }
   ];
 
   $("#slideshow").crossSlide(options, slides);
